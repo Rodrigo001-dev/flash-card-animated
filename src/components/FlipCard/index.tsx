93706eb0f725d1d86faaf6e42a097dc9302f16c5
Rodrigo <rodrigorael53@gmail.com>
2022-05-24 01:10:30 -0300

    initialize the animation on the flipCard

--- src/components/FlipCard/index.tsx
+++ src/components/FlipCard/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { Pressable } from 'react-native';
 
+/* 
+  o useSharedValue é um valor que vai ser armazenado e compartilhado com a
+  animação. Ele é parecido com o useState
+  o useAnimatedStyle é um recurso do reanimated para criar estilizações que vão
+  responder as animações, ou seja, a estilização animada
+*/
+import { useSharedValue, useAnimatedStyle } from 'react-native-reanimated';
+
 import { Card } from '../Card';
 import { styles } from './styles';
 
@@ -13,6 +21,20 @@ type Props = {
 }
 
 export function FlipCard({ data }: Props) {
+  // o flipPositionAnimated vai ser usado para armazenar uma informação 0 e 1
+  // eu vou usar essa estratégia porque eu quero saber se o card que está visível
+  // é o da frente ou de trás, se tiver o 0 é o da frente, se tiver 1 significa
+  // que é o card de trás que esta visível
+  const flipPositionAnimated = useSharedValue(0);
+
+  const frontCardAnimated = useAnimatedStyle(() => {
+    return {
+      transform: [{
+        rotateY: ''
+      }],
+    }
+  });
+  
   return (
     <Pressable onPress={() => { }}>
       <Card label={data.front} />
