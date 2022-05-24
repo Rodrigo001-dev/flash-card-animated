272a7e4ed8b0205b943c124d0b610138f6ceb8b9
Rodrigo <rodrigorael53@gmail.com>
2022-05-24 17:27:20 -0300

    making the card animation rotate

--- src/components/FlipCard/index.tsx
+++ src/components/FlipCard/index.tsx
@@ -6,8 +6,15 @@ import { Pressable } from 'react-native';
   animação. Ele é parecido com o useState
   o useAnimatedStyle é um recurso do reanimated para criar estilizações que vão
   responder as animações, ou seja, a estilização animada
+  o interpolate vai receber um vetor com as posições da animação e ele vai cuidar
+  para deixar a transição suave
 */
-import { useSharedValue, useAnimatedStyle } from 'react-native-reanimated';
+import { 
+  useSharedValue, 
+  useAnimatedStyle, 
+  interpolate,
+  withTiming
+} from 'react-native-reanimated';
 
 import { Card } from '../Card';
 import { styles } from './styles';
@@ -30,15 +37,52 @@ export function FlipCard({ data }: Props) {
   const frontCardAnimated = useAnimatedStyle(() => {
     return {
       transform: [{
-        rotateY: ''
+        /*
+          o flipPositionAnimated.value é para acessar o valor atual do
+          useSharedValue
+          o primeiro vetor[] é para colocar quais os valorer possíveis do meu
+          estado animado(useSharedValue) => [0, 1]
+          o segundo vetor[] eu coloco qual o novo valor que eu quero dar quando
+          chegar nesse ponto do estado da animação, se o valor atual do meu
+          estado animado é 0 então vai ficar em 0 mesmo, ou seja, o cartão da
+          frente, se for 1 então retaciona ele no eixo y em 180deg
+        */
+        rotateY: `${interpolate(flipPositionAnimated.value, [0, 1], [0, 180])}deg`
       }],
     }
   });
+
+  const backCardAnimated = useAnimatedStyle(() => {
+    return {
+      transform: [{
+        /*
+          o flipPositionAnimated.value é para acessar o valor atual do
+          useSharedValue
+          o primeiro vetor[] é para colocar quais os valorer possíveis do meu
+          estado animado(useSharedValue) => [0, 1]
+          o segundo vetor[] eu coloco qual o novo valor que eu quero dar quando
+          chegar nesse ponto do estado da animação, se o valor atual do meu
+          estado animado é 0 então retaciona ele no eixo y em 180deg, e quando
+          for 1 vai rotacionar 360deg
+        */
+        rotateY: `${interpolate(flipPositionAnimated.value, [0, 1], [180, 360])}deg`
+      }],
+    }
+  });
+
+  function handleFlipCard() {
+    // se o que estiver dentro do flipPositionAnimated for 0 eu vou trocar para
+    // 1 e se for 1 eu vou trocar para 0 
+    const newValue = flipPositionAnimated.value === 0 ? 1 : 0;
+    // com o withTiming eu estou aplicando o newValue no estado da animação e
+    // passando a duração
+    flipPositionAnimated.value = withTiming(newValue, { duration: 300 }); // 300ms
+  };
   
   return (
-    <Pressable onPress={() => { }}>
-      <Card label={data.front} />
-      <Card label={data.back} style={styles.back} />
+    <Pressable onPress={handleFlipCard}>
+      <Card label={data.front} style={frontCardAnimated} />
+      <Card label={data.back} style={[styles.back, backCardAnimated]} />
     </Pressable>
   );
 }
